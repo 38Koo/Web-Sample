b7e770557e5886652839f1632b057f763788ca90
38Koo <captain.marvel.mk@gmail.com>
2021-08-31 19:10:26 +0900

    制御処理着手

--- tools/numeron/numeron.js
+++ tools/numeron/numeron.js
@@ -1,8 +1,12 @@
 let number1; 
 let number2; 
 let number3; 
+let blnStart = false;
+let hiddenNumber = document.querySelector('.Number');
+let selectDigit = document.querySelector('.digit');
 
 const btnStart = document.querySelector('.btnstart').addEventListener('click', function(){
+    blnStart = true;
     number1 = getRandomInt(0,10);
     number2 = getRandomInt(0,10);
     number3 = getRandomInt(0,10);
@@ -11,11 +15,40 @@ const btnStart = document.querySelector('.btnstart').addEventListener('click', f
     console.log(number3);
 });
 
-const btnCheck = document.querySelector('.btncheck').addEventListener('click', function(){
-    let check = document.querySelector('.inputcheck').value;    
-    console.log(check.value);
+const select = document.querySelector('.digit').addEventListener('change', function(){
+    let strHidden = '';
+    if (selectDigit.value !== 0){
+        for (let i = 0; i < selectDigit.value; i++){
+            strHidden += '*';
+        }
+    }
+    console.log(selectDigit.value);
+    hiddenNumber.textContent = strHidden;
+    document.querySelector('.inputcheck').maxLength = selectDigit.value;
 });
 
+const btnCheck = document.querySelector('.btncheck').addEventListener('click', function(){
+    let intdigit = selectDigit.value;
+    //Startボタン押下後のみ処理を行う
+    if (blnStart === true){
+        let check = document.querySelector('.inputcheck').value;
+        if (isNaN(check) === true){
+            alert('数値を入力してください。');
+            return;            
+        }else if(check.length !== intdigit){
+            alert(`${intdigit}桁の数値を入力してください。`);
+            return;
+        }else{
+            //チェック処理
+            
+        }
+        console.log(check);
+    }
+});
+
+function checkNumber(number){
+
+}
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
